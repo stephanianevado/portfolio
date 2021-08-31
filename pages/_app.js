d88f7994d929f17daf4d69e71d1c45e360b06390
Stephania Nevado <stephania.arantxa@gmail.com>
2021-08-31 17:28:57 +0200

    add changes

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
-
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
