36b5f75591ae9a824d10c71c89a36990c46e2301
Stephania Nevado <stephania.arantxa@gmail.com>
2024-01-18 10:21:07 +0100

    here we go again

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = () => {
-  const nextConfig = {
-    compiler: {
-      styledComponents: true,
-    },
-  }
-  return nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
+  ...(() => {
+    const nextConfig = {
+      compiler: {
+        styledComponents: true,
+      },
+    }
+    return nextConfig
+  })(),
 }
